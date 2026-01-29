e190e035f1a557d1fde74cf66a8cf2287d363732
Singvard <89718326+Singvard@users.noreply.github.com>
2026-01-29 16:11:56 +0300

    Hw11 (#3)
    Co-authored-by: SSaratov <ssaratov@sportmasterlab.net>

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,61 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "system:env",
+        "classpath:config/${config.file}.properties",
+        "file:src/test/resources/config/${config.file}.properties"
+})
+
+public interface WebDriverConfig extends Config{
+    String CHROME = "CHROME";
+    String FULL_HD = "1920x1080";
+    String FALSE = "false";
+    String REMOTE_URL = "http://localhost:4444/wd/hub";
+    String BASE_URL = "https://www.saucedemo.com";
+    String TIMEOUT = "10000";
+
+    @Key("browser.name")
+    @DefaultValue(CHROME)
+    Browser browserName();
+
+    @Key("browser.version")
+    String browserVersion();
+
+    @Key("browser.size")
+    @DefaultValue(FULL_HD)
+    String browserSize();
+
+    @Key("remote.condition")
+    @DefaultValue(FALSE)
+    boolean isRemote();
+
+    @Key("remote.url")
+    @DefaultValue(REMOTE_URL)
+    String remoteUrl();
+
+    @Key("remote.username")
+    String remoteUsername();
+
+    @Key("remote.password")
+    String remotePassword();
+
+    @Key("enable.vnc")
+    @DefaultValue(FALSE)
+    boolean enableVnc();
+
+    @Key("enable.video")
+    @DefaultValue(FALSE)
+    boolean enableVideo();
+
+    @Key("base.url")
+    @DefaultValue(BASE_URL)
+    String baseUrl();
+
+    @Key("timeout")
+    @DefaultValue(TIMEOUT)
+    long timeout();
+}
---
+++ src/test/java/config/WebDriverProvider.java
@@ -0,0 +1,61 @@
+package config;
+
+import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class WebDriverProvider {
+    private static WebDriverConfig config;
+
+    public static void configure() {
+        getConfig();
+        Configuration.browser = config.browserName().toString();
+        var browserVersion = config.browserVersion();
+        if (Objects.nonNull(browserVersion)) {
+            Configuration.browserVersion = config.browserVersion();
+        }
+
+        Configuration.browserSize = config.browserSize();
+        Configuration.baseUrl = config.baseUrl();
+        Configuration.timeout = config.timeout();
+
+        if (config.isRemote()) {
+            Configuration.remote = getRemoteUrl();
+            Configuration.browserCapabilities = getCapabilities();
+        }
+
+
+    }
+
+    private static String getRemoteUrl() {
+        var url = config.remoteUrl();
+        if (Objects.nonNull(config.remoteUsername()) && Objects.nonNull(config.remotePassword())) {
+            url = url.replace("://", "://" + config.remoteUsername() + ":" + config.remotePassword() + "@");
+        }
+        return url;
+    }
+
+    private static DesiredCapabilities getCapabilities() {
+        var capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(
+                "selenoid:options",
+                Map.of(
+                        "enableVNC", config.enableVnc(),
+                        "enableVideo", config.enableVideo()
+                )
+        );
+
+        return capabilities;
+    }
+
+    public static WebDriverConfig getConfig() {
+        if (Objects.isNull(config)) {
+            config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+        }
+        return config;
+    }
+}
