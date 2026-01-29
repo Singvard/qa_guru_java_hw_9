c33ee3256b7d2be15e8721653ef801aec9599202
SSaratov <ssaratov@sportmasterlab.net>
2026-01-29 14:12:35 +0300

    WIP

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.WebDriverProvider;
@@ -14,6 +15,16 @@ public abstract class BaseTest {
     @BeforeAll
     static void configureSelenide() {
         WebDriverProvider.configure();
+        System.out.println("=== BASE TEST CONFIGURATION ===");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("OS: " + System.getProperty("os.name"));
+        System.out.println("User dir: " + System.getProperty("user.dir"));
+        System.out.println("=== FINAL SELENIDE CONFIG ===");
+        System.out.println("Browser: " + Configuration.browser);
+        System.out.println("Remote URL: " + Configuration.remote);
+        System.out.println("Base URL: " + Configuration.baseUrl);
+        System.out.println("Timeout: " + Configuration.timeout);
+        System.out.println("=============================");
     }
 
     @BeforeEach
