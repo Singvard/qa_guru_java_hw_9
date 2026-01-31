e27f361b2a31287a04efe1ae87708c992d9c06e8
SSaratov <ssaratov@sportmasterlab.net>
2026-01-31 15:11:34 +0300

    WIP

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,6 +6,7 @@ import config.WebDriverProvider;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import utils.Attach;
 
 public abstract class BaseTest {
@@ -13,13 +14,16 @@ public abstract class BaseTest {
 
     @BeforeAll
     static void configureSelenide() {
+        provider = new WebDriverProvider();
+        provider.configure();
+    }
+
+    @BeforeEach
+    void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(true)
                 .includeSelenideSteps(false));
-
-        provider = new WebDriverProvider();
-        provider.configure();
     }
 
     @AfterEach
